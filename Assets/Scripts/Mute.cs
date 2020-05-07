50830c6803c2e4703704e6798b5e7084d98fdb5b
aryansaraswat <46528597+aryansaraswat@users.noreply.github.com>
2020-05-08 01:20:45 +0530

    Initial commit

---
+++ Assets/Scripts/Mute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour
+{
+    public AudioSource _as;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    void Start()
+    {
+        _as = GetComponent<AudioSource>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _as.mute = !_as.mute;
+    }
+}
